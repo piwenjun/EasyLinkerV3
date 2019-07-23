93d6b6d7e7957d5de504ac4429a4d7b5a641d673
wwhai <751957846@qq.com>
2019-07-23 08:39:26 +0800

    调整设备数据类型，把表进行了合并

--- v3/src/main/groovy/com/easylinker/v3/modules/devicedata/model/DeviceData.groovy
+++ v3/src/main/groovy/com/easylinker/v3/modules/devicedata/model/DeviceData.groovy
@@ -3,26 +3,44 @@ package com.easylinker.v3.modules.devicedata.model
 import com.easylinker.framework.common.model.AbstractModel
 import com.easylinker.framework.common.model.DeviceType
 
-import javax.persistence.Entity
 import javax.persistence.EnumType
 import javax.persistence.Enumerated
 import javax.persistence.MappedSuperclass
-
-
-enum DeviceDataType {
-
-
-}
 
 /**
  * 设备数据抽象
  */
 @MappedSuperclass
 class DeviceData extends AbstractModel {
+    /**
+     * 数据
+     */
+    private String data
+    /**
+     * 单位
+     */
+    private String unit
+    /**
+     * 设备ID
+     */
     private String deviceSecurityId
+    /**
+     * 备注信息
+     */
     private String info
+    /**
+     * 数据类型（检索的时候用）
+     */
     @Enumerated(EnumType.STRING)
     private DeviceType deviceType
+
+    String getData() {
+        return data
+    }
+
+    void setData(String data) {
+        this.data = data
+    }
 
     DeviceType getDeviceType() {
         return deviceType
@@ -48,96 +66,4 @@ class DeviceData extends AbstractModel {
         this.deviceSecurityId = deviceSecurityId
     }
 
-
-}
-/**
- * 值类数据
- */
-@Entity
-class ValueData extends DeviceData {
-    //数据值
-    private float[] value
-    //数据单位
-    private String unit
-
-    float[] getValue() {
-        return value
-    }
-
-    void setValue(float[] value) {
-        this.value = value
-    }
-
-    String getUnit() {
-        return unit
-    }
-
-    void setUnit(String unit) {
-        this.unit = unit
-    }
-}
-
-/**
- * 文本类数据
- */
-@Entity
-class TextData extends DeviceData {
-
-    private String text
-
-    String getText() {
-        return text
-    }
-
-    void setText(String text) {
-        this.text = text
-    }
-}
-/**
- * 开关
- */
-@Entity
-class SwitchData extends DeviceData {
-
-    private String data
-
-    boolean getData() {
-        return data
-    }
-
-    void setData(String data) {
-        this.data = data
-    }
-}
-/**
- * 布尔数据
- */
-@Entity
-class BooleanData extends DeviceData {
-
-    private String data
-
-    boolean getData() {
-        return data
-    }
-
-    void setData(String data) {
-        this.data = data
-    }
-}
-/**
- * 文件类数据
- */
-@Entity
-class FileData extends DeviceData {
-
-    private String fileURL
-
-    String getFileURL() {
-        return fileURL
-    }
-
-    void setFileURL(String fileURL) {
-        this.fileURL = fileURL
-    }
 }
--- v3/src/main/groovy/com/easylinker/v3/modules/devicedata/service/DeviceDataService.groovy
+++ v3/src/main/groovy/com/easylinker/v3/modules/devicedata/service/DeviceDataService.groovy
@@ -1,8 +1,8 @@
 package com.easylinker.v3.modules.devicedata.service
 
 import com.easylinker.framework.common.model.DeviceType
-import com.easylinker.v3.modules.devicedata.dao.*
-import com.easylinker.v3.modules.devicedata.model.*
+import com.easylinker.v3.modules.devicedata.dao.DeviceDataRepository
+import com.easylinker.v3.modules.devicedata.model.DeviceData
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
@@ -11,15 +11,7 @@ import org.springframework.stereotype.Service
 @Service
 class DeviceDataService {
     @Autowired
-    BooleanDeviceDataRepository booleanDeviceDataRepository
-    @Autowired
-    FileDeviceDataRepository fileDeviceDataRepository
-    @Autowired
-    TextDeviceDataRepository textDeviceDataRepository
-    @Autowired
-    ValueDeviceDataRepository valueDeviceDataRepository
-    @Autowired
-    SwitchDeviceDataRepository switchDeviceDataRepository
+    DeviceDataRepository deviceDataRepository
 
     /**
      * 查询数据
@@ -30,50 +22,21 @@ class DeviceDataService {
      */
     Page<DeviceData> list(String deviceSecurityId, DeviceType deviceType, Pageable pageable) {
 
-        switch (deviceType) {
-            case DeviceType.VALUE:
-                return valueDeviceDataRepository.findAllByDeviceSecurityId(deviceSecurityId, pageable)
-            case DeviceType.TEXT:
-                return textDeviceDataRepository.findAllByDeviceSecurityId(deviceSecurityId, pageable)
-            case DeviceType.BOOLEAN:
-                return booleanDeviceDataRepository.findAllByDeviceSecurityId(deviceSecurityId, pageable)
-            case DeviceType.FILE:
-                return fileDeviceDataRepository.findAllByDeviceSecurityId(deviceSecurityId, pageable)
-            case DeviceType.SWITCH:
-                return switchDeviceDataRepository.findAllByDeviceSecurityId(deviceSecurityId, pageable)
-            default: return null
-        }
-
-    }
-    /**
-     *
-     * @param deviceSecurityId
-     * @param deviceType
-     * @param pageable
-     * @return
-     */
-
-    void save(DeviceData deviceData, DeviceType deviceType) {
-
-        switch (deviceType) {
-            case DeviceType.VALUE:
-                valueDeviceDataRepository.save(deviceData as ValueData)
-                break
-            case DeviceType.TEXT:
-                textDeviceDataRepository.save(deviceData as TextData)
-                break
-            case DeviceType.BOOLEAN:
-                booleanDeviceDataRepository.save(deviceData as BooleanData)
-                break
-            case DeviceType.FILE:
-                fileDeviceDataRepository.save(deviceData as FileData)
-                break
-            case DeviceType.SWITCH:
-                switchDeviceDataRepository.save(deviceData as SwitchData)
-                break
-            default: break
-        }
+        return deviceDataRepository.findAllByDeviceSecurityIdAndDeviceType(deviceSecurityId,deviceType, pageable)
 
     }
 
+/**
+ *
+ * @param deviceSecurityId
+ * @param deviceType
+ * @param pageable
+ * @return
+ */
+
+    void save(DeviceData deviceData) {
+
+        deviceDataRepository.save(deviceData)
+    }
 }
+
