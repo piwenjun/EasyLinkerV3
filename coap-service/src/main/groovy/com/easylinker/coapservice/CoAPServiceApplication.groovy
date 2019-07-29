2e1b7f5ec7abcffe81f498f3a3d0d4e934072bf4
wwhai <751957846@qq.com>
2019-07-29 22:37:27 +0800

    搞了Eureka网关但是跑不通

--- coap-service/src/main/groovy/com/easylinker/coapservice/CoAPServiceApplication.groovy
+++ coap-service/src/main/groovy/com/easylinker/coapservice/CoAPServiceApplication.groovy
@@ -1,9 +1,13 @@
-package com.easylinker.CoAPservice
+package com.easylinker.coapservice
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
 class CoAPServiceApplication {
 
     static void main(String[] args) {
--- gateway/src/main/groovy/com/easylinker/gateway/GatewayApplication.groovy
+++ gateway/src/main/groovy/com/easylinker/gateway/GatewayApplication.groovy
@@ -2,6 +2,13 @@ package com.easylinker.gateway
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.cloud.gateway.filter.GatewayFilter
+import org.springframework.cloud.gateway.filter.GatewayFilterChain
+import org.springframework.cloud.gateway.route.RouteLocator
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder
+import org.springframework.context.annotation.Bean
+import org.springframework.web.server.ServerWebExchange
+import reactor.core.publisher.Mono
 
 @SpringBootApplication
 class GatewayApplication {
@@ -10,4 +17,20 @@ class GatewayApplication {
         SpringApplication.run(GatewayApplication, args)
     }
 
+    @Bean
+    RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route({ r ->
+                    r.path("/baidu").uri("http://www.baidu.com").filter(new GatewayFilter() {
+                        @Override
+                        Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+                            println exchange.getRequest().getHeaders().get("token")
+                            return null
+                        }
+                    })
+                })
+                .route({ r -> r.path("/taobao").uri("http://www.taobao.com") })
+                .route({ r -> r.path("/hao123").uri("http://www.hao123.com") })
+                .build()
+    }
 }
