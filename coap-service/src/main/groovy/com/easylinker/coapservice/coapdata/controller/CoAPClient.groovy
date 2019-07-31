d42896bc8ccabaee015805632e656dda3af87a9c
wwhai <751957846@qq.com>
2019-07-31 23:43:29 +0800

    COAP完善

--- coap-service/src/main/groovy/com/easylinker/coapservice/coapdata/controller/CoAPClient.groovy
+++ coap-service/src/main/groovy/com/easylinker/coapservice/coapdata/controller/CoAPClient.groovy
@@ -1,48 +1,19 @@
 package com.easylinker.coapservice.coapdata.controller
 
-import org.eclipse.californium.core.CoapClient
-import org.eclipse.californium.core.CoapHandler
-import org.eclipse.californium.core.CoapResponse
-import org.eclipse.californium.core.Utils
-import org.eclipse.californium.core.coap.MediaTypeRegistry
+import com.mbed.coap.client.CoapClient
+import com.mbed.coap.client.CoapClientBuilder
+import com.mbed.coap.packet.CoapPacket
 
 class CoAPClient {
 
     static void main(String[] args) throws InterruptedException {
+        CoapClient client = CoapClientBuilder.newBuilder(new InetSocketAddress("localhost", 5683)).build()
 
-
-        CoapClient client = new CoapClient()
-        client.setURI("coap://localhost:5863/push?token=zq6NDc3sb6QmoQF1&data=112234")
-
-
-        println("GET:")
-        client.get(new CoapHandler() {
-            @Override
-            void onLoad(CoapResponse coapResponse) {
-                println coapResponse.payload.toString()
-
-            }
-
-            @Override
-            void onError() {
-                println "error"
-
-            }
-        })
-        println("POST:")
-
-        client.post(new CoapHandler() {
-
-            @Override
-            void onLoad(CoapResponse response) {
-                System.out.println(Utils.prettyPrint(response));
-            }
-
-            @Override
-            void onError() {
-
-            }
-        }, "T:1,C:23", MediaTypeRegistry.TEXT_PLAIN);
-
+        for (int i = 0; i < 20; i++) {
+            Thread.sleep(2)
+            CoapPacket coapResp = client.resource("/push") .sync().post().setPayload("hello")
+            println(coapResp?.payloadString)
+        }
+        //client.close()
     }
 }
--- coap-service/src/main/groovy/com/easylinker/coapservice/config/CoAPServerConfig.groovy
+++ coap-service/src/main/groovy/com/easylinker/coapservice/config/CoAPServerConfig.groovy
@@ -1,19 +1,18 @@
 package com.easylinker.coapservice.config
 
+import cn.hutool.http.HttpUtil
+import com.alibaba.fastjson.JSONObject
 import com.easylinker.framework.common.web.R
-import org.eclipse.californium.core.CoapResource
-import org.eclipse.californium.core.CoapServer
-import org.eclipse.californium.core.network.CoapEndpoint
-import org.eclipse.californium.core.network.config.NetworkConfig
-import org.eclipse.californium.core.server.resources.CoapExchange
-import org.eclipse.californium.elements.tcp.TcpServerConnector
+import com.mbed.coap.exception.CoapCodeException
+import com.mbed.coap.packet.Code
+import com.mbed.coap.server.CoapExchange
+import com.mbed.coap.server.CoapServer
+import com.mbed.coap.utils.CoapResource
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.data.mongodb.core.MongoTemplate
 import org.springframework.stereotype.Component
-
-import java.util.concurrent.Executors
 
 /**
  * @author wwhai* @date 2019/7/30 21:26
@@ -29,48 +28,85 @@ class CoAPServerConfig {
     String hostname
     @Value(value = '${coap.server.port}')
     int port
+    /**
+     * 添加CoAP协议处理器:考虑到COAP设备可能通过Get来传数据，所以两种形式都支持【用GET来提交数据在HTTP里面是不推荐的】
+     * COAP设备上传的数据，必须有以下几个字段:
+     * 1 token:String
+     * 2 data:JSON 长度小于1024KB
+     */
 
     @Bean
     CoapServer coapServer() {
-        final CoapServer server = new CoapServer()
-        server.addEndpoint(new CoapEndpoint(
-                new TcpServerConnector(new InetSocketAddress(hostname, port), 10, 20000),
-                NetworkConfig.getStandard()))
-
+        final CoapServer server = CoapServer.builder() .transport(port).build()
         /**
-         * 添加CoAP协议处理器:考虑到COAP设备可能通过Get来传数据，所以两种形式都支持【用GET来提交数据在HTTP里面是不推荐的】
-         * COAP设备上传的数据，必须有以下几个字段:
-         * 1 token:String
-         * 2 data:JSON 长度小于1024KB
+         * 【重要】此接口需要限制频率，目前先限制为每个客户端的上传时间间隔为2秒;具体
+         * 实现方法：push的时候，先redis.get(deviceSecurityId);如果没有拿到说明没有限制，数据放行;
+         * 否则拦截并且:redis.set(deviceSecurityId,2s)
+         * --------------------------------------------------------------------------------------------
+         * 1 消息到来的时候 先检查【token】的合法性【这里需要调用V3的一个接口：/coap/isValid?token=[token]】
+         *  [问题：一个请求会经过两次微服务，势必会影响性能，考虑如何优化这部分]
+         * 2 如果【token】不合法，就不予处理
+         * 3 如果【token】合法，保存数据到数据库
+         * --------------------------------------------------------------------------------------------
+         * 备注：关于这个Token的认证形式，初步先用最简单的数据库查找认证【但是真的很消耗性能】
+         * 后面准备用非对称加密算法来认证
+         * --------------------------------------------------------------------------------------------
          */
-        server.add(new CoapResource("push") {
+
+        server.addRequestHandler("/push", new CoapResource() {
             @Override
-            void handleGET(CoapExchange exchange) {
-                if (exchange.getRequestText().length() > 1024) {
-                    exchange.respond(R.error("Data too long").toJSONString())
-                }
-                String token = exchange.getQueryParameter("token")
-                String data = exchange.getQueryParameter("data")
-                if ((!token) || (!data)) {
-                    exchange.respond(R.error("Missing argument").toJSONString())
-
-                }
-
-
-            };
+            void get(CoapExchange coapExchange) throws CoapCodeException {
+            }
 
             @Override
-            void handlePOST(CoapExchange exchange) {
-                System.out.println(exchange.getRequestPayload())
+            void post(CoapExchange coapExchange) throws CoapCodeException {
+                if (!coapExchange.getRequestBodyString()) {
+                    return
+                }
+                if (coapExchange.getRequestBodyString().length() > 1024) {
+                    coapExchange.setResponseBody(R.error("Data too long!Most 1024KB!").toJSONString())
+                    coapExchange.setResponseCode(Code.C205_CONTENT)
+                    coapExchange.sendResponse()
+                } else {
+                    try {
+                        /**
+                         * String token【必填】
+                         * String data【必填】
+                         * String unit【选填】
+                         * String deviceSecurityId【必填】
+                         * String info【选填】
+                         */
 
+                        //JSON 可能解析异常
+                        JSONObject dataJson = JSONObject.parseObject(coapExchange.request.payloadString)
+                        //数据入库请求
+
+                        String token = dataJson.getString("token")
+//                        String deviceSecurityId = dataJson.getString("deviceSecurityId")
+//                        String data = dataJson.getString("token")
+//                        String unit = dataJson.getString("unit")
+//                        String info = dataJson.getString("info")
+                        //请求【V3】保存数据
+                        if (!token) {
+                            coapExchange.setResponseBody(R.ok("Invalid token!").toJSONString())
+                            coapExchange.setResponseCode(Code.C205_CONTENT)
+                            coapExchange.sendResponse()
+                        }
+                        //推送
+                        String result = HttpUtil.post("V3的地址", dataJson)
+                        coapExchange.setResponseBody(R.ok(result).toJSONString())
+                        coapExchange.setResponseCode(Code.C205_CONTENT)
+                        coapExchange.sendResponse()
+                    } catch (Exception e) {
+                        coapExchange.setResponseBody(R.ok("Data format mast be define json!").toJSONString())
+                        coapExchange.setResponseCode(Code.C205_CONTENT)
+                        coapExchange.sendResponse()
+                    }
+
+                }
             }
         })
-        Executors.newSingleThreadExecutor().execute(new Runnable() {
-            @Override
-            void run() {
-                server.start()
-            }
-        })
+        server.start()
         return server
     }
 }
