323778c913e58bf10d8cdb6bb31349d4a99dc2f9
wwhai <751957846@qq.com>
2019-08-07 14:45:23 +0800

    其中一个项目找不到classpath，我怀疑是IDEA的问题，搞了一个早上不行。只能删除重新新建，果然可以了，可能是玄学。

--- mqtt-service/src/main/groovy/com/easylinker/mqttservice/MqttServiceApplication.groovy
+++ mqtt-service/src/main/groovy/com/easylinker/mqttservice/MqttServiceApplication.groovy
@@ -1,20 +1,15 @@
 package com.easylinker.mqttservice
 
 
-import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
 @SpringBootApplication
-class MqttServiceApplication implements CommandLineRunner {
+class MqttServiceApplication {
 
 
     static void main(String[] args) {
         SpringApplication.run(MqttServiceApplication, args)
     }
 
-    @Override
-    void run(String... args) throws Exception {
-
-    }
 }
