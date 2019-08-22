5601954467633769a5cd0da6b13e5eecebaa5d17
zhangguokang <guokang.zhang@aqara.com>
2019-08-22 21:11:50 +0800

    产品新增和修改

---
+++ v3/src/main/groovy/com/easylinker/v3/modules/product/form/AddProductForm.groovy
@@ -0,0 +1,38 @@
+package com.easylinker.v3.modules.product.form
+
+
+
+
+class AddProductForm {
+    /**
+     * 厂家
+     */
+    private String factory
+    /**
+     * 型号
+     */
+    private String model
+    /**
+     * 名称
+     */
+    private String name
+    /**
+     * 备注
+     */
+    private String info
+
+    /**
+     * 场景id
+     */
+    private String sceneSecurityId;
+
+    /**
+     * 创建时间
+     */
+    private Date createTime;
+
+    /**
+     * 用户id
+     */
+    private String appUserSecurityId;
+}
